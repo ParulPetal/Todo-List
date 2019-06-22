bcf6f4c98e4a5e95c9500ca5df1524708468a96e
ParulPetal <parulpetal550@gmail.com>
2019-06-22 13:00:03 +0530

    Yeahh Shopping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShoppingList';
+
+
+  items = ["Mango", "Potato", "Paneer"];
+
+  newItem = "";
+  pushItem = function () {
+
+    if (this.newItem != "") {
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+
+  removeItem= function(index){
+this.items.splice(index,1);
+
+
+  }
 }
